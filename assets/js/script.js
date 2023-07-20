ca6be845293413e446d6742279969a5861a6ba81
João Pedro Costa <104322778+jpcostadev@users.noreply.github.com>
2023-07-20 13:37:08 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,43 @@
-// const formulario = document.getElementById("form");
-// const botao = document.querySelectorAll(".btn-pedir");
+const formulario = document.getElementById("form");
+const botao = document.querySelectorAll(".btn-pedir");
 
-// function formularioEnviado(resposta) {
-//   console.log(resposta);
-//   if (resposta.ok) {
-//     formulario.innerHTML =
-//       "<p class='msg-enviada'>Pedido Realizado com <span>sucesso1</span>, só aguardar!</p>";
-//   } else {
-//     formulario.innerHTML =
-//       " <p class='msg-nao-enviada'> <span>Erro</span> ao realizar o pedido, chame um atendente </p>";
-//   }
-// }
+function formularioEnviado(resposta) {
+  console.log(resposta);
+  if (resposta.ok) {
+    formulario.innerHTML =
+      "<p class='msg-enviada'>Pedido Realizado com <span>sucesso1</span>, só aguardar!</p>";
+  } else {
+    formulario.innerHTML =
+      " <p class='msg-nao-enviada'> <span>Erro</span> ao realizar o pedido, chame um atendente </p>";
+  }
+}
 
-// function enviarFormulario(event) {
-//   event.preventDefault();
-//   const botaoClicado = event.submitter;
-//   const form = new FormData(formulario);
+function enviarFormulario(event) {
+  event.preventDefault();
+  const botaoClicado = event.submitter;
+  const form = new FormData(formulario);
 
-//   const dados = {
-//     nome: form.get("nome"),
-//     mesa: form.get("mesa"),
-//     whatsapp: form.get("whatsapp"),
-//   };
+  const dados = {
+    nome: form.get("nome"),
+    mesa: form.get("mesa"),
+    whatsapp: form.get("whatsapp"),
+  };
 
-//   if (botaoClicado) {
-//     if (dados.nome === "" || dados.whatsapp === "" || dados.mesa === "") {
-//       alert("Preencha os campos Nome, Mesa, Whatsapp");
-//       return;
-//     } else if (dados.whatsapp.length < 7 || isNaN(dados.whatsapp)) {
-//       alert("Digite um número válido");
-//       return;
-//     }
+  if (botaoClicado) {
+    if (dados.nome === "" || dados.whatsapp === "" || dados.mesa === "") {
+      alert("Preencha os campos Nome, Mesa, Whatsapp");
+      return;
+    } else if (dados.whatsapp.length < 7 || isNaN(dados.whatsapp)) {
+      alert("Digite um número válido");
+      return;
+    } // Aqui, vamos usar o fetch para enviar o formulário para o servidor
 
-//   }
-// }
+    fetch("./enviar.php", {
+Hide quoted text
+      method: "POST",
+      body: form,
+    }).then(formularioEnviado);
+  }
+}
 
-// formulario.addEventListener("submit", enviarFormulario);
+formulario.addEventListener("submit", enviarFormulario);
